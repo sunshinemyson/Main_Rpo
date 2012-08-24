dd6f4bcede50053c5dfe8ab0c4f1274b510213e6
Sven_ThinkPad <sunshinemyson@163.com>
2012-08-24 17:05:14 +0800

    Commit main_rpo without commit Submodule first

--- Main_Rpo.pl
+++ Main_Rpo.pl
@@ -1,9 +1,13 @@
 use lib './SubModule_ALib';
 use lib './SubModule_BLib';
+
 use pkg_b;
 use pkg_a;
+use another;
 pkg_a::say();
 pkg_b::say();
 
 pkg_a::newFunc();
 pkg_b::newFunc();
+
+another::say();
